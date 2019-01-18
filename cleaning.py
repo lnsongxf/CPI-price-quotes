f6791e8dfc648a1ba4f0cb64dfb76c72f5bba20b
Rahat Siddique <35703640+rahatsiddique@users.noreply.github.com>
2019-01-18 17:24:05 +0000

    Create cleaning.py
    useful cleaning commands

---
+++ cleaning.py
@@ -0,0 +1,7 @@
+#TITLE: CLEANING THE MASTER DATASET
+
+#commands i may need for the cleaning 
+
+# count: df.count()
+#drop if : df = df[df.line_race != 0
+#dropping missing: df = df[df.line_race.notnull()]
